a21204a71bd3ac15e9567b5c4a50343d24db958e
NinjaPratap <darshpratap@gmail.com>
2017-12-07 20:34:25 +0530

    Change First Page
    Signed-off-by: NinjaPratap <darshpratap@gmail.com>

---
+++ app/src/main/java/com/pratap/ninja/baka/Activities/ResourceActivity.java
@@ -0,0 +1,10 @@
+package com.pratap.ninja.baka.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by darsh on 7/12/17.
+ */
+
+public class ResourceActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/pratap/ninja/baka/MainActivity.java
+++ app/src/main/java/com/pratap/ninja/baka/MainActivity.java
@@ -4,26 +4,38 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.pratap.ninja.baka.Activities.AdvertisementActivity;
 import com.pratap.ninja.baka.Activities.FirstYearNotesActivity;
+import com.pratap.ninja.baka.Activities.ResourceActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    ImageView iv_resources, iv_adv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void NotesMethod(View v){
-        this.startActivity(new Intent(MainActivity.this, FirstYearNotesActivity.class));
-    }
-    public void PapersMethod(View v){
 
-    }
-    public void AssignMethod(View v){
+        iv_adv = findViewById(R.id.main_advertisement);
+        iv_resources = findViewById(R.id.main_resources);
 
-    }
-    public void EbookMethod(View v){
+
+        iv_resources.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ResourceActivity.class));
+            }
+        });
+
+        iv_adv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AdvertisementActivity.class));
+            }
+        });
 
     }
 }
