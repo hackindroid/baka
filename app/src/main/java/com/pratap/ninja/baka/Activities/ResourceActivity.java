995f8a5a5ee789fe31e7d935f793a6739357fefe
NinjaPratap <darshpratap@gmail.com>
2017-12-07 20:39:31 +0530

    Create start page and Resouces,Adv Page
    Signed-off-by: NinjaPratap <darshpratap@gmail.com>

--- app/src/main/java/com/pratap/ninja/baka/Activities/ResourceActivity.java
+++ app/src/main/java/com/pratap/ninja/baka/Activities/ResourceActivity.java
@@ -1,10 +1,20 @@
 package com.pratap.ninja.baka.Activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.pratap.ninja.baka.R;
 
 /**
  * Created by darsh on 7/12/17.
  */
 
 public class ResourceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resources);
+    }
 }
